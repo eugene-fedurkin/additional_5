2fad246f595cb6b0cd16edc19301d5a51d564b10
eugene-fedurkin <eugene.fedurkin@gmail.com>
2017-09-19 15:57:16 +0300

    implemented check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    for (let item of bracketsConfig) {
+      if (str[i] === item[0] && str[i + 1] === item[1]) {
+        str = str.slice(0, i) + str.slice(i + 2);
+        i = -1;
+        if (str === '') return true;
+      }
+    }
+  }
+  return false;
 }
